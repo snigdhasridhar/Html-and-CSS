e30e7c7ad868e092be5798bed0e20d067adecb86
Snigdha Sridhar <snigdhasridhar1@gmail.com>
2024-09-19 22:05:08 +0530

    Add files via upload

---
+++ javascript./hello.js
@@ -0,0 +1,22 @@
+function ment(){
+
+var name=document.getElementById("first_name").value 
+document.getElementById("para").innerHTML="your name is "+name
+var last_name=document.getElementById("last_name").value
+document.getElementById("para2").innerHTML="your name is "+last_name
+var date=document.getElementById("dob").value 
+document.getElementById("para3").innerHTML="your date of birth is "+date
+var email=document.getElementById("email").value
+document.getElementById("para4").innerHTML="your email is "+ email
+var pass=document.getElementById("pass").value
+document.getElementById("para5").innerHTML="your password is "+ pass
+var gender=document.getElementById("gender").value
+document.getElementById("para6").innerHTML="you are a"+" "+gender
+
+
+
+
+
+
+
+}
--- javascript./index.js
+++ javascript./index.js
@@ -1,1 +1,10 @@
-
+function java_script(){
+    var name=document.getElementById("snigdha").value 
+// alert(name)
+// console.log(name)
+document.getElementById("java").innerHTML="<b>"+name+"</b>"
+document.getElementById("c").innerText="<b>"+name+"</b>"
+var naming=document.getElementById("password").value
+// document.getElementById("pass").innerHTML="<b>"+naming+"</b>"
+alert(naming)
+}
